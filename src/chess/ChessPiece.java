e0d5011c8c9981ec1a68eb19366ae19495f24897
antoniocarlos-bc <antoniocarlos_bc@outlook.com>
2020-11-22 21:41:49 -0300

    Retomando o programa

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,31 @@
+package chess;
+
+import boardGame.Board;
+import boardGame.Piece;
+import boardGame.Position;
+
+public abstract class ChessPiece extends Piece{
+	
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position)
+	{
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p !=null && p.getColor() != color;		
+		
+	}
+}
